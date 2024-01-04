8bb7f7b449f418ee684b2d2f929780ee565c0435
Tharindu B. Hewage <tharindu.b.hewage@gmail.com>
2024-01-04 21:53:46 +1100

    Add deployment doc

---
+++ deployment/add-stack-user.sh
@@ -0,0 +1,5 @@
+# adds a user named `stack` and grant required permissions.
+
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+sudo chmod +x /opt/stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
