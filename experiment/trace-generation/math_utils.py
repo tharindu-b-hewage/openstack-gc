b600d91e79afe754a12ab7123980c1d7d91a1715
Tharindu B. Hewage <tharindu.b.hewage@gmail.com>
2024-01-18 18:54:03 +1100

    Add trace gen tested for 4-min test

--- experiment/trace-generation/math_utils.py
+++ experiment/trace-generation/math_utils.py
@@ -1,6 +1,10 @@
 import scipy.stats
 
 
-def pickRandom(dist):
-    kde = scipy.stats.gaussian_kde(dist)
-    return kde.resample(size=1)
+def pick_random(dst):
+    is_uniform = len(set(dst)) == 1
+    if is_uniform:
+        return dst[0]
+    kde = scipy.stats.gaussian_kde(dst)
+    sample = kde.resample(size=1)
+    return sample[0][0]
--- experiment/trace-generation/trace-generator.py
+++ experiment/trace-generation/trace-generator.py
@@ -2,8 +2,20 @@ import sys
 import time
 
 import pandas as pd
-from math_utils import pickRandom
+from math_utils import pick_random
 from external import dispatch
+
+# 1 - normalized azure trace csv
+# 2 - starting time
+# 3 - end time
+# 4 - max number of requests at a time
+# 5 - max lifetime of a vm
+# 6 - max vcpu of an instant
+
+# Example:
+# python3 trace-generator.py 4-min-test/nrl_azure_packing_2020.csv 0.819502315018326 0.8208333 5 0.00277778 12
+# csv file is generated for 4 minutes (0.00277778 days) between 0.819502315018326 and 0.8208333. max req. set to 5 and
+# max lifetime is the duration of experiment.
 
 nrl_trace_file = sys.argv[1]
 t_start = float(sys.argv[2])
@@ -21,10 +33,10 @@ df = df[t_start <= df['time']]
 df = df[df['time'] <= t_stop]
 
 
-def generate_reg_rqs(rq_count, row, time, type, bucket):
+def generate_rqs(rq_count, row, time, type, bucket):
     for rq in range(rq_count):
-        lifetime = pickRandom(dst=row['lifetime_distribution'][0]) * max_lft
-        vcpu = round(pickRandom(dst=row['vcpu_distribution'][0]) * max_vcpu_cnt)
+        lifetime = pick_random(dst=eval(row['lifetime_distribution'][0])) * max_lft
+        vcpu = round(pick_random(dst=eval(row['vcpu_distribution'][0])) * max_vcpu_cnt)
         if vcpu > 0:
             bucket.append({
                 'name': 'VM-' + str(time) + '-' + type + '-' + str(rq),
@@ -36,19 +48,16 @@ def generate_reg_rqs(rq_count, row, time, type, bucket):
 
 t_s = df['time'].values
 for idx, t in enumerate(t_s):
-    print("time: ", t)
     row = df.loc[df['time'] == t].to_dict('list')
-    print('row', row)
 
     vm_rqs = []
     total_rq_cnt = row['request_count'][0] * max_rq_cnt
     reg_rq_cnt = round(total_rq_cnt * row['regular_vm_count'][0])
     evct_rq_cnt = round(total_rq_cnt * row['evictable_vm_count'][0])
-    print('total_rq_cnt',total_rq_cnt, 'reg_rq_cnt',reg_rq_cnt, evct_rq_cnt)
     if reg_rq_cnt > 0:
-        generate_reg_rqs(rq_count=reg_rq_cnt, row=row, time=t, type='regular', bucket=vm_rqs)
+        generate_rqs(rq_count=reg_rq_cnt, row=row, time=t, type='regular', bucket=vm_rqs)
     if evct_rq_cnt > 0:
-        generate_reg_rqs(rq_count=evct_rq_cnt, row=row, time=t, type='evictable', bucket=vm_rqs)
+        generate_rqs(rq_count=evct_rq_cnt, row=row, time=t, type='evictable', bucket=vm_rqs)
 
     dispatch(vm_rqs=vm_rqs)
 
