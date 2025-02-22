0d39fd1445803cfca49033937969ae8a9bb45731
Tharindu B. Hewage <tharindu.b.hewage@gmail.com>
2025-02-22 14:12:58 +1100

    add vm eviction for proto packing

--- experiments/vm-packing/analysis/conduct_analysis.py
+++ experiments/vm-packing/analysis/conduct_analysis.py
@@ -1,6 +1,7 @@
 import sys
 
 from get_nLT import analyse_nlt
+from get_vm_evictions import analyze_vm_evictions
 
 if len(sys.argv) == 1:
     exp_type = "debug"
@@ -8,3 +9,4 @@ else:
     exp_type = sys.argv[1]
 
 analyse_nlt(exp_type=exp_type)
+analyze_vm_evictions(exp_type=exp_type)
